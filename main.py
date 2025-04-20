e13a7a2504b4ce4f4dc8327726e79663bc2b79a1
arslanAta <arsyakmar@gmail.com>
2025-04-20 19:52:35 +0500

    Added get and post

--- main.py
+++ main.py
@@ -1,46 +1,31 @@
+from pydantic import BaseModel,Field
+from typing import  List,Optional
+from fastapi import  FastAPI
 import uvicorn
-from fastapi import FastAPI,HTTPException
-from pydantic import  BaseModel
 
 app = FastAPI()
 
+class AddTodoSchema(BaseModel):
+    text: str = Field(max_length=150)
 
-class NewBook(BaseModel):
-    title:str
-    author:str
+class TodoSchema(AddTodoSchema):
+    id: int
 
-books = [
-    {
-        "id":1,
-        "name":"My book1",
-        "author":'Someone1'
-    },
-    {
-        "id":2,
-        "name":"My book2",
-        "author":'Someone2'
-    }
-]
 
-@app.get('/books',tags=['Books'],summary="Get all books")
-def root():
-    return books
+todos:Optional[List[TodoSchema]] = []
 
-@app.get("/books/{book_id}",tags=['Books'],summary="Get book by id")
-def get_book(book_id:int):
-    for book in books:
-        if book['id'] == book_id:
-            return book
-    raise  HTTPException(status_code=404)
+@app.get('/todos')
+def get_todos():
+    return todos
 
-@app.post('/books',tags=['Books'])
-def add_book(new_book:NewBook):
-    books.append({
-        "id":len(books)+1,
-        "title":new_book.title,
-        "author":new_book.author
-    })
-    return books
+@app.post('/todos')
+def add_todo(new_todo:AddTodoSchema):
+    todo = TodoSchema(
+        id=len(todos) + 1 | 1,
+        text=new_todo.text
+    )
+    todos.append(todo)
+    return todos
 
 if __name__ == "__main__":
     uvicorn.run("main:app",reload=True)
