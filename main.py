cfdfa4b9622f7f026cac1dcec6cb109662732e34
arslanAta <arsyakmar@gmail.com>
2025-04-21 09:40:31 +0500

    Added delete and update endpoints

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel,Field
 from typing import  List,Optional
-from fastapi import  FastAPI
+from fastapi import FastAPI, HTTPException
 import uvicorn
 
 app = FastAPI()
@@ -14,18 +14,43 @@ class TodoSchema(AddTodoSchema):
 
 todos:Optional[List[TodoSchema]] = []
 
-@app.get('/todos')
+@app.get('/todos',tags=['Todo management'])
 def get_todos():
     return todos
 
-@app.post('/todos')
+@app.get('/todos/{todo_id}',tags=['Todo management'])
+def get_todo_by_id(todo_id:int):
+    for todo in todos:
+        if todo.id == todo_id :
+            return todo
+    raise HTTPException(status_code=404)
+
+
+@app.post('/todos',tags=['Todo management'])
 def add_todo(new_todo:AddTodoSchema):
     todo = TodoSchema(
-        id=len(todos) + 1 | 1,
+        id=len(todos) + 1,
         text=new_todo.text
     )
     todos.append(todo)
     return todos
+
+@app.patch('/todos/{todo_id}',tags=['Todo management'])
+def update_todo(todo_id:int,updated_todo:AddTodoSchema):
+    for todo in todos:
+        if todo.id == todo_id:
+            todo.text = updated_todo.text
+            return todo
+    raise HTTPException(status_code=404)
+
+@app.delete('/todos/{todo_id}',tags=['Todo management'])
+def delete_todo(todo_id:int):
+    for todo in todos:
+        if todo.id == todo_id:
+            todos.remove(todo)
+            return todos
+    raise HTTPException(status_code=404)
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app",reload=True)
